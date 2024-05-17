b490c665be217d1fc9fde3399c02c94abd8a58fc
khanhdt <khanhdt@smartosc.com>
2024-05-17 17:25:31 +0700

    add api and openapi

--- main.go
+++ main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"context"
 	"database/sql"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
+	"net/http"
 	db "simple_bank/db/sqlc"
 	"simple_bank/internal/service/api"
 	"simple_bank/internal/service/gapi"
 	pb "simple_bank/proto/pb/proto"
 	"simple_bank/util"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 )
 
@@ -25,7 +30,9 @@ func main() {
 		log.Fatal("cannot connect db:", err)
 	}
 	store := db.NewStore(conn)
+	go runGWServer(config, store)
 	runGrpcServer(config, store)
+
 }
 
 func runGinServer(config util.Config, store db.Store) {
@@ -56,6 +63,46 @@ func runGrpcServer(config util.Config, store db.Store) {
 	}
 	log.Printf("start gRPC server on %s", config.GRPCServerAddress)
 	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot serve:", err)
+	}
+}
+
+func runGWServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	grpcMux := runtime.NewServeMux(jsonOption)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err = pb.RegisterUserServiceHandlerServer(ctx, grpcMux, server)
+	if err != nil {
+		log.Fatal("cannot register server:", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	fs := http.FileServer(http.Dir("./doc/swagger"))
+	mux.Handle("/swagger/", http.StripPrefix("/swagger", fs))
+
+	listener, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot listen:", err)
+	}
+	log.Printf("start http gateway server on %s", config.HTTPServerAddress)
+	err = http.Serve(listener, mux)
 	if err != nil {
 		log.Fatal("cannot serve:", err)
 	}
